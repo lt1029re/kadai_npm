37b2a938830514d2612fe81810e6e872d6be4cf8
lt1029re <lt1029re@ed.ritsumei.ac.jp>
2020-01-15 05:34:59 +0000

    課題を追加

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const http = require('http');
+const server = http.createServer((req, res) => {
+res.setHeader('Access-Control-Allow-Origin', '*');
+    res.write('hello')
+            const num = Math.random()
+            if(num < 0.5) {
+                res.write('吉です')
+            } else if(num < 0.8) {
+                res.write('中吉です')
+            } else if (num < 0.95) {
+                res.write('大吉です')
+            } else {
+                res.write('きょうです')
+            }    
+    
+res.end()
+        })
+    
+server.listen(4000);
+console.log('The server is listenning...')
